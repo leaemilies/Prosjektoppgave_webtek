c6a9e0392213f1917611dabf3769d4335b829f7a
kasper karslen <kasperkarlsen03@gmail.com>
2025-10-13 14:22:55 +0200

    spilljs

---
+++ spil.js
@@ -0,0 +1,33 @@
+// spill basecode 
+
+let forsok = 5;
+let riktigSvar="Oslo";
+let lengdeElement = document.getElementById("lengde");
+    lengdeElement.textContent = "*".repeat(riktigSvar.length);
+
+function sjekksvar() {
+
+    const brukerSvar = document.getElementById("svarfelt").value;
+    const resultat = document.getElementById("resultat");
+
+    if (brukerSvar === riktigSvar.toLowerCase()) {
+        result.textContent = "Riktig svar!";
+        result.style.color = "green";
+        document.getElementById("sjekk").disabled = true;
+    } else {
+        forsok--;
+        result.textContent = "Feil svar, du har " + forsok + " forsøk igjen.";
+        result.style.color = "red";
+        document.getElementById("svarfelt").value = "";
+
+        if (forsok === 0) {
+            result.textContent = "Du har brukt opp alle forsøkene.";
+            document.getElementById("sjekk").disabled = true;
+        }
+    }
+  
+}
+window.onload = function() {
+    document.getElementById("sjekk").onclick = sjekksvar;
+}
+
