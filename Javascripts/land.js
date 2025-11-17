10c1dc7c6ba5d6300486076aca2f4fa7805ba9e5
kasper karslen <kasperkarlsen03@gmail.com>
2025-11-17 14:10:03 +0100

    pyramide

--- Javascripts/land.js
+++ Javascripts/land.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "&#x1F3AD;&#x1F35D;&#x1F355;&#x1F3DB;&#xFE0F;","&#x1F3D4;&#xFE0F;&#x26F7;&#xFE0F;&#x231A;&#x1F36B;","&#x1F30B;&#x2708;&#xFE0F;&#x1F3DD;&#xFE0F;&#x1F409;",
     "&#x1F993;&#x1F992;&#x1F3DE;&#xFE0F;&#x2600;&#xFE0F;","&#x1F3DF;&#xFE0F;&#x1F965;&#x26BD;&#x1F3DD;&#xFE0F;","&#x1F3D4;&#xFE0F;&#x1F999;&#x1F954;&#x2600;&#xFE0F;",
     "&#x1F54C;&#x1F386;&#x1F95C;&#x2600;&#xFE0F;","&#x1F54C;&#x1F418;&#x1F35B;&#x1F3B6;","&#x1F3DD;&#xFE0F;&#x1F334;&#x1F3C4;&#x200D;&#x2640;&#xFE0F;&#x1F34D;",
-    "&#x26F7;&#xFE0F;&#x1F341;&#x1F341;&#x1F3D2;","&#x1F411;&#x1F3C9;&#x1F5FB;&#x1F377;","&#x1F9CA;&#x1F427;&#x26F7;&#xFE0F;&#x1F30C;","&#x1F54C;&#x1F54C;&#x1F42A;&#x1F3DC;&#xFE0F;",
+    "&#x26F7;&#xFE0F;&#x1F341;&#x1F341;&#x1F3D2;","&#x1F411;&#x1F3C9;&#x1F5FB;&#x1F377;","&#x1F9CA;&#x1F427;&#x26F7;&#xFE0F;&#x1F30C;","&#9650;&#x1F54C;&#x1F42A;&#x1F3DC;&#xFE0F;",
     "&#x1F958;&#x1F3B6;&#x26BD;&#x1F3DD;&#xFE0F;","&#x1F3A1;&#x1F3AD;&#x2614;&#xFE0F;&#x1FAD6;","&#x1F32E;&#x1F389;&#x1F30B;&#x1FA85;","&#x1F3DB;&#xFE0F;&#x1F3DD;&#xFE0F;&#x1F347;&#x1F3FA;",
     "&#x1F981;&#x1F377;&#x26F0;&#xFE0F;&#x1F3C4;&#x200D;&#x2642;&#xFE0F;","&#x1F969;&#x1F483;&#x26BD;&#x26F0;&#xFE0F;","&#x1F962;&#x1F35C;&#x1F3B6;&#x1F3F0;","&#x1F6D5;&#x1F418;&#x1F35C;&#x1F334;",
     "&#x1F337;&#x1F6B2;&#x1F9C0;&#x1F3A8;","&#x1F377;&#x26F5;&#x1F483;&#x1F3DD;&#xFE0F;"
